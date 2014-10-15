45fa463ea8e168e675516a228d588c6e89847154
Dmitry Smal <mialinx@gmail.com>
2014-10-15 14:47:46 +0400

    init script

---
+++ init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# vim 
+[ -e $HOME/.vim ] || ln -s $HOME/dotfiles/.vim $HOME/.vim 
+[ -d $HOME/.vim/backup ] || mkdir $HOME/.vim/backup
+touch $HOME/.vimrc
+grep dotfiles/.vimrc $HOME/.vimrc || echo "source ~/dotfiles/.vimrc" >> $HOME/.vimrc
+
+# bash
+touch $HOME/.bashrc
+grep dotfiles/.colours $HOME/.bashrc || echo "source ~/dotfiles/.colours" >> $HOME/.bashrc
+grep dotfiles/.bashrc $HOME/.bashrc || echo "source ~/dotfiles/.bashrc" >> $HOME/.bashrc
+
+# screen
+[ -e $HOME/.screenrc ] || ln -s $HOME/dotfiles/.screenrc $HOME/.screenrc
